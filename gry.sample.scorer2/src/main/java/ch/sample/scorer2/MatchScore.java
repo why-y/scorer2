0f4dae153a6980e3d3a94c0cd458d491b8529bb5
Yves Gross <why-y@gmx.net>
2018-06-12 17:12:46 +0200

    support also if B scores on match level

--- gry.sample.scorer2/src/main/java/ch/sample/scorer2/MatchScore.java
+++ gry.sample.scorer2/src/main/java/ch/sample/scorer2/MatchScore.java
@@ -5,60 +5,88 @@ import java.util.List;
 
 public class MatchScore {
 
+    enum Mode {BEST_OF_3, BEST_OF_5}
+
+    private final Mode mode;
     private final List<Set> terminatedSets;
     private final Set currentSet;
     private final Game currentGame;
 
-    private MatchScore(final List<Set> terminatedSets, Set currentSet, Game currentGame) {
+    private MatchScore(Mode mode, final List<Set> terminatedSets, Set currentSet, Game currentGame) {
+        this.mode = mode;
         this.terminatedSets = terminatedSets;
         this.currentSet = currentSet;
         this.currentGame = currentGame;
     }
 
     public static MatchScore start() {
-        return new MatchScore(new ArrayList<>(5), Set.withTiebreak().start(), Game.start());
+        return start(Mode.BEST_OF_3);
+    }
+
+    public static MatchScore start(Mode mode) {
+        return new MatchScore(mode, new ArrayList<>(mode==Mode.BEST_OF_3 ? 3 : 5), Set.withTiebreak().start(), Game.start());
     }
 
     public MatchScore scoreA() {
-        if (matchIsOver()) {
-            throw new AlreadyTerminatedException("Must not score on terminated Match! Final score is " + printFullScore());
-        }
+        assertMatchNotOver();
         Game updatedGame = currentGame.scoreA();
-        if(updatedGame.isOver()){
-            Game newGame = Game.start();
-            Set updatedSet = currentSet.scoreA();
-            if(updatedSet.isOver()) {
-                List<Set> updatedSets = addTerminatedSet(updatedSet);
-                Set newSet = Set.withoutTiebreak().start();
-                return new MatchScore(updatedSets, newSet, newGame);
-            }
-            return new MatchScore(terminatedSets, updatedSet, newGame);
+        if(updatedGame.isOver()) {
+            return updateSetAndStartNewGame(currentSet.scoreA());
         }
-        return new MatchScore(terminatedSets, currentSet, currentGame.scoreA());
+        return new MatchScore(mode, terminatedSets, currentSet, updatedGame);
+    }
+
+    public MatchScore scoreB() {
+        assertMatchNotOver();
+        Game updatedGame = currentGame.scoreB();
+        if(updatedGame.isOver()) {
+            return updateSetAndStartNewGame(currentSet.scoreB());
+        }
+        return new MatchScore(mode, terminatedSets, currentSet, updatedGame);
     }
 
     public boolean matchIsOver() {
         return aHasWon() || bHasWon();
     }
 
+    public String printFullScore() {
+        return matchIsOver()
+                ? String. format("%s - Game, Set and Match Player %s", printTerminatedSets(), aHasWon() ? "A" : "B")
+                : String.format("%s%s-%s", printTerminatedSets(), printSet(currentSet), currentGame.print());
+    }
+
     private boolean aHasWon() {
-        return terminatedSets.stream().filter(set -> set.aHasWon()).count() >= 2;
+        return terminatedSets.stream().filter(set -> set.aHasWon()).count() >= noOfSetsToWin();
     }
 
     private boolean bHasWon() {
-        return terminatedSets.stream().filter(set -> set.bHasWon()).count() >= 2;
+        return terminatedSets.stream().filter(set -> set.bHasWon()).count() >= noOfSetsToWin();
+    }
+
+    private MatchScore updateSetAndStartNewGame(final Set updatedSet) {
+        return updatedSet.isOver() ?
+                updateTerminatedSetsAndStartNewSet(updatedSet) :
+                new MatchScore(mode, terminatedSets, updatedSet, Game.start());
+    }
+
+    private MatchScore updateTerminatedSetsAndStartNewSet(final Set terminatedSet) {
+        return new MatchScore(mode, addTerminatedSet(terminatedSet), Set.withoutTiebreak().start(), Game.start());
+    }
+
+    private void assertMatchNotOver() {
+        if (matchIsOver()) {
+            throw new AlreadyTerminatedException("Must not score on terminated Match! Final score is " + printFullScore());
+        }
+    }
+
+    private int noOfSetsToWin() {
+        return this.mode==Mode.BEST_OF_3 ? 2 : 3;
     }
 
     private List<Set> addTerminatedSet(final Set terminatedSet) {
         List<Set> updatedTerminatedSets = new ArrayList<>(terminatedSets);
         updatedTerminatedSets.add(terminatedSet);
         return updatedTerminatedSets;
-    }
-
-    public String printFullScore() {
-        return matchIsOver()
-                ? String. format("%s - Game, Set and Match Player %s", printTerminatedSets(), aHasWon() ? "A" : "B")
-                : String.format("%s%s-%s", printTerminatedSets(), printSet(currentSet), currentGame.print());
     }
 
     private String printTerminatedSets() {
--- gry.sample.scorer2/src/test/java/ch/sample/scorer2/MatchScoreTest.java
+++ gry.sample.scorer2/src/test/java/ch/sample/scorer2/MatchScoreTest.java
@@ -2,6 +2,8 @@ package ch.sample.scorer2;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.Is.is;
+import ch.sample.scorer2.MatchScore.Mode;
+import static ch.sample.scorer2.MatchScore.Mode.*;
 
 import org.junit.Test;
 
@@ -9,21 +11,50 @@ import static org.junit.Assert.*;
 
 public class MatchScoreTest {
 
-    private final static MatchScore SCORE_6_0_5_0_40_0 = MatchScore.start()
-            .scoreA().scoreA().scoreA().scoreA() // 1:0
-            .scoreA().scoreA().scoreA().scoreA() // 2:0
-            .scoreA().scoreA().scoreA().scoreA() // 3:0
-            .scoreA().scoreA().scoreA().scoreA() // 4:0
-            .scoreA().scoreA().scoreA().scoreA() // 5:0
-            .scoreA().scoreA().scoreA().scoreA() // 6:0
-            .scoreA().scoreA().scoreA().scoreA() // 6:0 1:0
-            .scoreA().scoreA().scoreA().scoreA() // 6:0 2:0
-            .scoreA().scoreA().scoreA().scoreA() // 6:0 3:0
-            .scoreA().scoreA().scoreA().scoreA() // 6:0 4:0
-            .scoreA().scoreA().scoreA().scoreA() // 6:0 5:0
-            .scoreA().scoreA().scoreA();         // 6:0 5:0 40:0
+    private final MatchScore scoreTo_6_0_6_0_5_0_40_0() {
+        return scoreTo_6_0_5_0_40_0(BEST_OF_5).scoreA() // 6:0 6:0
+                .scoreA().scoreA().scoreA().scoreA() //  6:0 6:0 1:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 6:0 2:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 6:0 3:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 6:0 4:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 6:0 5:0
+                .scoreA().scoreA().scoreA();         // 6:0 6:0 5:0 40:0
 
-    @Test
+    }
+
+    private final MatchScore scoreTo_6_0_5_0_40_0(Mode mode) {
+        return MatchScore.start(mode)
+                .scoreA().scoreA().scoreA().scoreA() // 1:0
+                .scoreA().scoreA().scoreA().scoreA() // 2:0
+                .scoreA().scoreA().scoreA().scoreA() // 3:0
+                .scoreA().scoreA().scoreA().scoreA() // 4:0
+                .scoreA().scoreA().scoreA().scoreA() // 5:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 1:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 2:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 3:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 4:0
+                .scoreA().scoreA().scoreA().scoreA() // 6:0 5:0
+                .scoreA().scoreA().scoreA();         // 6:0 5:0 40:0
+    }
+
+    private final MatchScore scoreTo_0_6_0_5_0_40(Mode mode) {
+        return MatchScore.start(mode)
+                .scoreB().scoreB().scoreB().scoreB() // 0:1
+                .scoreB().scoreB().scoreB().scoreB() // 0:2
+                .scoreB().scoreB().scoreB().scoreB() // 0:3
+                .scoreB().scoreB().scoreB().scoreB() // 0:4
+                .scoreB().scoreB().scoreB().scoreB() // 0:5
+                .scoreB().scoreB().scoreB().scoreB() // 0:6
+                .scoreB().scoreB().scoreB().scoreB() // 0:6 0:1
+                .scoreB().scoreB().scoreB().scoreB() // 0:6 0:2
+                .scoreB().scoreB().scoreB().scoreB() // 0:6 0:3
+                .scoreB().scoreB().scoreB().scoreB() // 0:6 0:4
+                .scoreB().scoreB().scoreB().scoreB() // 0:6 0:5
+                .scoreB().scoreB().scoreB();         // 0:6 0:5 0:40
+    }
+
+        @Test
     public void startMatch_scoreIs_0_0__0_0() {
         assertThat("the initial score of a match must be [0:0]-0:0", MatchScore.start().printFullScore(), equalTo("[0:0]-0:0"));
     }
@@ -52,24 +83,46 @@ public class MatchScoreTest {
 
     @Test(expected = AlreadyTerminatedException.class)
     public void given_MatchIsOverScoreAthrowsExc() {
-        SCORE_6_0_5_0_40_0.scoreA() // Match over!!
+        scoreTo_6_0_5_0_40_0(BEST_OF_3).scoreA() // Match over!!
         .scoreA();
     }
 
     @Test
     public void given_6_0_5_0__40_0_matchIsNotOver() {
-        assertThat(SCORE_6_0_5_0_40_0.matchIsOver(), is(false));
+        assertThat(scoreTo_6_0_5_0_40_0(BEST_OF_3).matchIsOver(), is(false));
     }
 
     @Test
     public void given_6_0_5_0__40_0_matchIsOver() {
-        assertThat(SCORE_6_0_5_0_40_0.scoreA().matchIsOver(), is(true));
+        assertThat(scoreTo_6_0_5_0_40_0(BEST_OF_3).scoreA().matchIsOver(), is(true));
     }
 
     @Test
     public void given_6_0_5_0__40_0_aScoresGets_aWon_6_0_6_0() {
-        assertThat(SCORE_6_0_5_0_40_0.scoreA()
+        assertThat(scoreTo_6_0_5_0_40_0(BEST_OF_3).scoreA()
                 .printFullScore(), equalTo("[6:0][6:0] - Game, Set and Match Player A"));
+    }
+
+    @Test
+    public void given_0_6_0_5_0_40_bScores_gets_bWon_0_6_0_6() {
+        assertThat(scoreTo_0_6_0_5_0_40(BEST_OF_3).scoreB().printFullScore(), equalTo("[0:6][0:6] - Game, Set and Match Player B"));
+    }
+
+    @Test
+    public void given_bestOfFive_6_0_6_0_isNotOver() {
+        assertThat(scoreTo_6_0_5_0_40_0(BEST_OF_5).scoreA().matchIsOver(), is(false));
+    }
+
+    @Test
+    public void given_bestOfFive_6_0_6_0_5_0_40_0_isNotOver() {
+        assertThat(scoreTo_6_0_6_0_5_0_40_0()
+                .matchIsOver(), is(false));
+    }
+
+    @Test
+    public void given_bestOfFive_6_0_6_0_6_0_isOver() {
+        assertThat(scoreTo_6_0_6_0_5_0_40_0().scoreA()
+                .matchIsOver(), is(true));
     }
 
 }
